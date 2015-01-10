73e024f023517068ae5c6a7b054b83ece43dad95
Concretechris <Concretechris@users.noreply.github.com>
2015-01-10 14:04:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
       $("#reset").click(function(){
         $(".container").remove();
         $(".cell").remove();
-        //var col = prompt("Please enter the number of columns: ");
-        //var rows = prompt("Please enter the number rows: ");
+        var col = prompt("Please enter the number of columns: ");
+        var rows = prompt("Please enter the number rows: ");
         //$("body").append("<div class="container"><div class="cell"/><div class="cell"/><div class="cell"/><div class = "cell"/></div>");
       });
 });
