4b2ec5f0965d82d30bee3138203716c4c01cffa5
Concretechris <Concretechris@users.noreply.github.com>
2015-01-10 14:27:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
         
         for (var i = 1; i <= rows; i++)
         {
-          $(".new_container").append("<div><p>What the fuck?!?</p></div>");
+          $(".new_container").append("<div class = "container"><p>What the fuck?!?</p></div>");
         }
       });
 });
