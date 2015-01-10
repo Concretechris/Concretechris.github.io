fe36c10df1adaf7371959ac7ba5103e983c724bd
Concretechris <Concretechris@users.noreply.github.com>
2015-01-10 15:55:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+    $("#reset").click(function(){
+    $(".container").remove();
+    $(".cell").remove();
+    var col = prompt("Please enter the number of columns: ");
+    var rows = prompt("Please enter the number rows: ");
+    
+    for (var i = 1; i <= rows; i++)
+    {
+      $d = $("<div class = 'container'></div>");
+      $(".new_container").append($d);
+    }
+    for (var j = 1; j <= col; j++)
+    {
+      $c = $("<div class = 'cell'></div>");
+      $(".container").append($c);
+    }
+  });
   $("h1").mouseenter(function() {
     $("h1").fadeTo("slow", .5);
   });
@@ -13,23 +30,6 @@ $(document).ready(function() {
         $(".cell").height("200px");
         $(".cell").width("200px");
   });
-      $("#reset").click(function(){
-        $(".container").remove();
-        $(".cell").remove();
-        var col = prompt("Please enter the number of columns: ");
-        var rows = prompt("Please enter the number rows: ");
-        
-        for (var i = 1; i <= rows; i++)
-        {
-          $d = $("<div class = 'container'></div>");
-          $(".new_container").append($d);
-        }
-        for (var j = 1; j <= col; j++)
-        {
-          $c = $("<div class = 'cell'></div>");
-          $(".container").append($c);
-        }
-      });
 });
 
 //add spans function and global div/span variables
