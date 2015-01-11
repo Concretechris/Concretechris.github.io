9919e6f1539983344a67178611726d81e16dd770
Concretechris <Concretechris@users.noreply.github.com>
2015-01-11 05:03:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 $(document).ready(function() {
-    for (var i = 1; i <= rows; i++)
-    {
-      $d = $("<div class = 'container'></div>");
-      $(".new_container").append($d);
-      $(".container").height(960/rows - 2);
-    }
-    for (var j = 1; j <= col; j++)
-    {
-      $c = $("<div class = 'cell'></div>");
-      $(".container").append($c);
-      $(".cell").width(960/col - 4);
-      $(".cell").height(960/col - 4);
-    }
-      $(".cell").mouseenter(function(){
-        $(this).addClass("black");
-    });
     
 $("#reset").click(function(){
     $(".container").remove();
