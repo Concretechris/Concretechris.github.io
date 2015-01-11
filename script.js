811e17aa5e6950d2ba9c1989d893bdedaf65c8a4
Concretechris <Concretechris@users.noreply.github.com>
2015-01-11 05:05:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ $("#reset").click(function(){
     var col = prompt("Please enter the number of columns: ");
     var rows = prompt("Please enter the number rows: ");
     
+    $(".container").height(960/rows - 2);
+    $(".cell").width(960/col - 4);
+    $(".cell").height(960/col - 4);
+    
     for (var i = 1; i <= rows; i++)
     {
       $d = $("<div class = 'container'></div>");
       $(".new_container").append($d);
-      $(".container").height(960/rows - 2);
     }
     for (var j = 1; j <= col; j++)
     {
       $c = $("<div class = 'cell'></div>");
       $(".container").append($c);
-      $(".cell").width(960/col - 4);
-      $(".cell").height(960/col - 4);
     }
       $(".cell").mouseenter(function(){
         $(this).addClass("black");
